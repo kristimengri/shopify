45302ca7107a6f56dc2d40e1a58519c695451449
kristimengri <kristimengri@gmail.com>
2024-10-15 17:49:28 +0200

    added order - review relation

--- src/main/java/com/kent/gmail/com/runtime/model/Order.java
+++ src/main/java/com/kent/gmail/com/runtime/model/Order.java
@@ -1,6 +1,13 @@
 package com.kent.gmail.com.runtime.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
 
 @Entity
-public class Order extends Base {}
+public class Order extends Base {
+
+    @OneToMany(mappedBy = "order")
+    private List<Review> reviews;
+}
